76358012e43f0d9a61c3316d09a0158bdb3079a8
gopal <vgopalooty@gmail.com>
2018-03-03 11:24:11 +0530

    cc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path : 'home',component : HomeComponent }
+,
+  {path: 'dashboard', component :DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+ }
